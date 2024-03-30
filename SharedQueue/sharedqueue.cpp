8df91bf4c446ab8ad8c0528ec7ec4db4788474d0
nataliaRabelo <nataliabruno@id.uff.br>
2024-03-30 11:45:37 -0300

    ok

--- SharedQueue/sharedqueue.cpp
+++ SharedQueue/sharedqueue.cpp
@@ -1,4 +1,5 @@
-#include "pch.h"
+// shared_queue.cpp
+
 #include <map>
 #include <queue>
 #include <string>
@@ -8,12 +9,12 @@ std::map<std::string, std::queue<std::string>> queues;
 std::mutex mutex;
 
 extern "C" {
-    __declspec(dllexport) void enqueue(const char* queueName, const char* message) {
+    void enqueue(const char* queueName, const char* message) {
         std::lock_guard<std::mutex> lock(mutex);
         queues[queueName].push(std::string(message));
     }
 
-    __declspec(dllexport) const char* dequeue(const char* queueName) {
+    const char* dequeue(const char* queueName) {
         static std::string message;
         {
             std::lock_guard<std::mutex> lock(mutex);
